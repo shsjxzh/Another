8f1435ff25bc8cf8c2c41d0045bf68d48b3a92c8
shsjxzh <1927045093@qq.com>
2018-06-09 17:19:52 +0800

    fix bug for push callee

--- src/shsjxzh/compiler/BackEnd/AssemblePrinter.java
+++ src/shsjxzh/compiler/BackEnd/AssemblePrinter.java
@@ -16,7 +16,7 @@ public class AssemblePrinter implements IRVisitor {
     private Set<String> BBVisit = new HashSet<>();
     private boolean definingGlobal = false;
     private Function curFunc;
-    private Stack<PhysicalRegister> calleeSaveReg = new Stack<>();
+    private List<PhysicalRegister> calleeSaveReg = new LinkedList<>();
 
     //int localVarSize = 0;
 
@@ -120,18 +120,22 @@ public class AssemblePrinter implements IRVisitor {
         //for callee save
         //node.usedCalleePhyReg
 
-        calleeSaveReg.clear();
-        for (PhysicalRegister calleeReg : node.usedCalleePhyReg) {
-            this.out.print("\tpush "); AssemblePrint(calleeReg); this.out.println();
-            calleeSaveReg.push(calleeReg);
-        }
         //although it may be wasteful, to be convenient I need it...
         this.out.println("\tsub rsp, " + node.funcLocalVarRegs.size() * 8);
 
+        calleeSaveReg.clear();
+        for (PhysicalRegister calleeReg : node.usedCalleePhyReg) {
+            this.out.print("\tpush "); AssemblePrint(calleeReg); this.out.println();
+            calleeSaveReg.add(calleeReg);
+        }
+
         AssemblePrint(node.getStartBB());
 
-        while(!calleeSaveReg.empty()){
-            this.out.print("\tpop "); AssemblePrint(calleeSaveReg.pop()); this.out.println();
+        List<PhysicalRegister> reversecallSaveReg = calleeSaveReg.subList(0, calleeSaveReg.size());
+        Collections.reverse(reversecallSaveReg);
+
+        for (PhysicalRegister phyReg : reversecallSaveReg) {
+            this.out.print("\tpop "); AssemblePrint(phyReg); this.out.println();
         }
 
         //to ensure safety
@@ -461,8 +465,11 @@ public class AssemblePrinter implements IRVisitor {
             this.out.print("\tmov rax, "); AssemblePrint(node.getReturnValue()); this.out.println();
         }
 
-        while(!calleeSaveReg.empty()){
-            this.out.print("\tpop "); AssemblePrint(calleeSaveReg.pop()); this.out.println();
+        List<PhysicalRegister> reversecallSaveReg = calleeSaveReg.subList(0, calleeSaveReg.size());
+        Collections.reverse(reversecallSaveReg);
+
+        for (PhysicalRegister phyReg : reversecallSaveReg) {
+            this.out.print("\tpop "); AssemblePrint(phyReg); this.out.println();
         }
 
         this.out.println("\tleave");
@@ -939,7 +946,7 @@ public class AssemblePrinter implements IRVisitor {
         if (node.hasPhysicalReg()) AssemblePrint(node.trueReg);
         else{
             if (curFunc.funcLocalVarRegs.containsKey(node.getName())) {
-                this.out.print(("qword [rbp - " + (curFunc.localVarPos.get(node.getName()) + 8 + curFunc.usedCalleePhyReg.size() * 8) + "] "));  //
+                this.out.print(("qword [rbp - " + (curFunc.localVarPos.get(node.getName()) + 8) + "] "));  //
             }
 
             else if (curFunc.funcParams.containsKey(node.getName())) {
